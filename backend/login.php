a9407ec29984bc7513e34b12f0925c3cffa23f40
Ali Mansour <alialhadimansour21@gmail.com>
2024-03-14 20:07:13 +0200

    implement the login api

---
+++ backend/login.php
@@ -0,0 +1,31 @@
+<?php
+include "./connection.php";
+$txt_username = isset($_POST['username']) ? $_POST['username'] : null;
+$txt_email = isset($_POST['email']) ? $_POST['email'] : null;
+$txt_password = isset($_POST['password']) ? $_POST['password'] : null;
+$is_email = filter_var($txt_email, FILTER_VALIDATE_EMAIL);
+
+if ($is_email) {
+    $query = $conn->prepare('SELECT * FROM users WHERE email = ?');
+    $query->bind_param('s', $txt_email);
+} else {
+    $query = $conn->prepare('SELECT * FROM users WHERE username = ?');
+    $query->bind_param('s', $txt_username);
+}
+$query->execute();
+$query->store_result();
+$query->bind_result($id, $username, $email, $password);
+$check_user = $query->fetch();
+if ($check_user == 0) {
+    $response['status'] = "Account not found";
+} else {
+    if (password_verify($txt_password, $password)) {
+        $response['status'] = "logged in";
+        $response['id'] = $id;
+        $response['username'] = $username;
+        $response['email'] = $email;
+    } else {
+        $response['status'] = "incorrect credentials";
+    }
+}
+echo json_encode($response);
