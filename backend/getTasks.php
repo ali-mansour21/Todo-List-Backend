acfb7df73b7820327bfb901fb37b4676a1fd56bb
Ali Mansour <alialhadimansour21@gmail.com>
2024-03-14 20:28:01 +0200

    implement the read tasks api

---
+++ backend/getTasks.php
@@ -0,0 +1,21 @@
+<?php
+include "./connection.php";
+$user_id = isset($_POST['user_id']) ? $_POST['user_id'] : null;
+
+
+$query = $conn->prepare('select * from tasks where user_id = ?');
+$query->bind_param('i', $user_id);
+$query->execute();
+$query->store_result();
+$query->bind_result($id, $title, $description, $user_id, $score);
+$response = [];
+while ($query->fetch()) {
+    $task = [
+        'id' => $id,
+        'title' => $title,
+        'description' => $description,
+        'score' => $score
+    ];
+    $response[] = $task;
+}
+echo json_encode($response);
